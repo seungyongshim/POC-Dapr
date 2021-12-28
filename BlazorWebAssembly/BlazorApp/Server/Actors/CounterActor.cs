b40c67e221e877a7a446b7cb14965fe90518d371
심승용 <seungyongshim@nexon.co.kr>
2021-12-28 12:04:52 +0900

    3333

--- BlazorWebAssembly/BlazorApp/Server/Actors/CounterActor.cs
+++ BlazorWebAssembly/BlazorApp/Server/Actors/CounterActor.cs
@@ -1,23 +1,41 @@
 using Dapr.Actors.Runtime;
 
-namespace BlazorApp.Server.Actors
+namespace BlazorApp.Server.Actors;
+
+public class CounterActor : Actor, ICounterActor
 {
-
-    public class CounterActor : Actor, ICounterActor
+    public CounterActor(ActorHost host) : base(host)
     {
-        public CounterActor(ActorHost host) : base(host)
-        {
 
-        }
+    }
 
-        public async Task<int> GetCountAsync() =>
-            await StateManager.TryGetStateAsync<int>("Counter") switch
-            {
-                var ret when ret.HasValue is true => ret.Value,
-                _ => 0
-            };
+    public async Task<int> GetCountAsync()
+    {
 
-        public Task<int> AddCounterAsync() =>
-            StateManager.AddOrUpdateStateAsync<int>("Counter", 1, (s, v) => 1 + v);
+        var q = from x in StateManager.TryGetStateAsync<CounterActorState>("CounterActorState").ToAff()
+                from b in Eff(() => x.Value switch
+                {
+                    CounterActorState c => c.Count,
+                    _ => 0
+                })
+                select b;
+
+        var r = await q.Retry(Schedule.Recurs(10) | Schedule.Fibonacci(100 * milliseconds)).Run();
+
+        return r.ThrowIfFail();
+    }
+
+    public async Task<int> AddCounterAsync()
+    {
+        var q = from x in StateManager.AddOrUpdateStateAsync("CounterActorState",
+                                                             new CounterActorState(1),
+                                                             (s, v) => v with { Count = 1 + v.Count }).ToAff()
+                from b in Eff(() => x.Count)
+                select b;
+
+        var r = await q.Retry(Schedule.Recurs(10) | Schedule.Fibonacci(100 * milliseconds)).Run();
+        return r.ThrowIfFail();
     }
 }
+
+public record CounterActorState(int Count);
--- BlazorWebAssembly/BlazorApp/Server/Controllers/BackCounterController.cs
+++ BlazorWebAssembly/BlazorApp/Server/Controllers/BackCounterController.cs
@@ -3,33 +3,36 @@ using Dapr.Actors.Client;
 using BlazorApp.Server.Actors;
 using Dapr.Actors;
 
-namespace BlazorApp.Server.Controllers
+namespace BlazorApp.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BackCounterController : Controller
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class BackCounterController : Controller
+    public BackCounterController(IActorProxyFactory actorProxy)
     {
-        public BackCounterController(IActorProxyFactory actorProxy)
-        {
-            CounterActor = actorProxy.CreateActorProxy<ICounterActor>(new ActorId("1"), "CounterActor");
-        }
+        CounterActor = actorProxy.CreateActorProxy<ICounterActor>(new ActorId("1"), "CounterActor");
+    }
 
-        public ICounterActor CounterActor { get; }
+    public ICounterActor CounterActor { get; }
 
-        [HttpGet]
-        public async Task<int> Get()
-        {
-            var q = CounterActor.GetCountAsync().ToAff().Retry(Schedule.Recurs(10) | Schedule.Fibonacci(100 * milliseconds));
-                    
-            var ret = await q.Run();
-            return ret.ThrowIfFail();
-        }
+    [HttpGet]
+    public async Task<int> Get()
+    {
+        var q = from x in CounterActor.GetCountAsync().ToAff()
+                select x;
 
-        [HttpGet("Add")]
-        public async Task<int> Add()
-        {
-            var ret = await CounterActor.AddCounterAsync();
-            return ret;
-        }
+        var r = await q.Retry(Schedule.Recurs(10) | Schedule.Fibonacci(100 * milliseconds)).Run();
+        return r.ThrowIfFail();
+    }
+
+    [HttpGet("Add")]
+    public async Task<int> Add()
+    {
+        var q = from x in CounterActor.AddCounterAsync().ToAff()
+                select x;
+
+        var r = await q.Retry(Schedule.Recurs(10) | Schedule.Fibonacci(100 * milliseconds)).Run();
+        return r.ThrowIfFail();
     }
 }
